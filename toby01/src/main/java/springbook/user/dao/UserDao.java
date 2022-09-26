659670e3bb7fe79cacc90477877605ad209e6bef
jjkim90 <jaejun8613@gmail.com>
2022-09-26 11:19:58 +0900

    Update toby01

---
+++ toby01/src/main/java/springbook/user/dao/UserDao.java
@@ -0,0 +1,76 @@
+package springbook.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import springbook.user.domain.User;
+
+public class UserDao {
+	
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		UserDao dao = new UserDao();
+		
+		User user = new User();
+		user.setId("javalover");
+		user.setName("박자바");
+		user.setPassword("java123");
+		
+		dao.add(user);
+		
+		System.out.println(user.getId() + "등록 성공");
+		
+		User user2 = dao.get(user.getId());
+		System.out.println(user2.getName());
+		System.out.println(user2.getPassword());
+		
+		System.out.println(user2.getId() + "조회 성공");
+	}
+
+	public void add(User user) throws ClassNotFoundException, SQLException {
+		Connection c = getConnection();
+		
+		PreparedStatement ps = c.prepareStatement(
+				"insert into users(id, name, password) values(?,?,?)");
+		ps.setString(1, user.getId());
+		ps.setString(2, user.getName());
+		ps.setNString(3, user.getPassword());
+		
+		ps.executeUpdate();
+		
+		ps.close();
+		c.close();
+	}
+	
+	public User get(String id) throws ClassNotFoundException, SQLException {
+		Connection c = getConnection();
+		
+		PreparedStatement ps = c.prepareStatement(
+				"select * from users where id = ?");
+		ps.setString(1, id);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		User user = new User();
+		user.setId(rs.getString("id"));
+		user.setName(rs.getString("name"));
+		user.setPassword(rs.getString("password"));
+		
+		rs.close();
+		ps.close();
+		c.close();
+		
+		return user;
+	}
+	
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("org.mariadb.jdbc.Driver");
+		Connection c = DriverManager.getConnection(
+				"jdbc:mysql://localhost/toby", "toby", "toby");
+		return c;
+	}
+	
+}
